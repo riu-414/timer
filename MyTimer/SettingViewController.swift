1a41a1bcb6d01582e7e2b10b2a6723f76348db45
riu-414 <mande_e@ezweb.ne.jp>
2022-01-23 16:12:37 +0900

    part1

---
+++ MyTimer/SettingViewController.swift
@@ -0,0 +1,69 @@
+//
+//  SettingViewController.swift
+//  MyTimer
+//
+//  Created by 大杉祐弥 on 2022/01/15.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var timerSettingPicker: UIPickerView!
+    
+    let settingArray : [Int] = [10,20,30,40,50,60]
+    let settingKey = "timer_value"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        timerSettingPicker.delegate = self
+        timerSettingPicker.dataSource = self
+        
+        let settings = UserDefaults.standard
+        let timerValue = settings.integer(forKey: settingKey)
+        
+        for row in 0..<settingArray.count {
+            if settingArray[row] == timerValue {
+                timerSettingPicker.selectRow(row, inComponent: 0, animated: true)
+            }
+        }
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return settingArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(settingArray[row])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let settings = UserDefaults.standard
+        settings.setValue(settingArray[row], forKey: settingKey)
+        settings.synchronize()
+    }
+    
+    @IBAction func decisionButton(_ sender: Any) {
+        _ = navigationController?.popViewController(animated: true)
+    }
+    
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MyTimer/ViewController.swift
+++ MyTimer/ViewController.swift
@@ -8,12 +8,82 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var countDownLavel: UILabel!
+    
+    var timer : Timer?
+    var count = 0
+    let settingKey = "timer_value"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let settings = UserDefaults.standard
+        settings.register(defaults: [settingKey:10])
+        
     }
+    
+    @IBAction func settingButton(_ sender: Any) {
+        if let nowTimer = timer {
+            if nowTimer.isValid == true {
+                nowTimer.invalidate()
+            }
+        }
+        performSegue(withIdentifier: "goSetting", sender: nil)
+    }
+    
 
-
+    @IBAction func startButton(_ sender: Any) {
+        print("スタート")
+        if let nowTimer = timer {
+            if nowTimer.isValid == true {
+                return
+            }
+        }
+        timer = Timer.scheduledTimer(timeInterval: 1.0,
+                                     target: self,
+                                     selector: #selector(self.timerInterrupt(_:)),
+                                     userInfo: nil,
+                                     repeats: true)
+    }
+    
+    
+    @IBAction func stopButton(_ sender: Any) {
+        print("ストップ")
+        if let nowTimer = timer {
+            if nowTimer.isValid == true {
+                nowTimer.invalidate()
+            }
+        }
+    }
+    
+    func displayUpdate() -> Int {
+        let setting = UserDefaults.standard
+        let timerValue = setting.integer(forKey: settingKey)
+        let remainCount = timerValue - count
+        countDownLavel.text = "残り\(remainCount)秒"
+        return remainCount
+    }
+    
+    @objc func timerInterrupt(_ timer:Timer) {
+        count += 1
+        if displayUpdate() <= 0 {
+            count = 0
+            timer.invalidate()
+            
+            let alertController = UIAlertController(title: "終了", message: "タイマー終了時間です", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(defaultAction)
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        count = 0
+        _ = displayUpdate()
+    }
+    
 }
 
